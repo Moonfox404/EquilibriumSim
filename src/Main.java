a5c28eb31166f7351925b8da839f50346d89baf0
Xuan Pham <hpham024@uottawa.ca>
2023-12-25 15:58:57 -0500

    added timer to update simulation status at fixed rate

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class Main {
+
+    static void updateStatus(){
+
+    }
+
+    static void runSim(){
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+                                      @Override
+                                      public void run() {
+                                          updateStatus();
+                                      }
+                                  },
+                0, 500);
+    }
+
+    public static void main(String[] args) {
+        
     }
 }

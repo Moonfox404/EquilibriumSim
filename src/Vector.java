0f8e1ce64162e6b0eab1157ac95ed3fa3985a360
Xuan Pham <hpham024@uottawa.ca>
2023-12-25 20:12:09 -0500

    added add and subtract methods

--- src/Vector.java
+++ src/Vector.java
@@ -39,4 +39,32 @@ public class Vector {
     public void setAngle(double val){
         angle = val;
     }
+
+    // other methods
+
+    public Vector add(Vector other){
+        double x_component = (value * Math.cos(angle)) + (other.getValue() * Math.cos(other.getAngle()));
+        double y_component = (value * Math.sin(angle)) + (other.getValue() * Math.sin(other.getAngle()));
+
+        double res_magnitude = Math.sqrt(Math.pow(x_component, 2) + Math.pow(y_component, 2));
+        double res_angle = Math.atan(Math.abs(x_component) / Math.abs(y_component));
+
+        if(x_component < 0){
+            if(y_component >= 0){
+                res_angle = Math.PI - res_angle;
+            } else {
+                res_angle = Math.PI + res_angle;
+            }
+        } else if(y_component < 0){
+            res_angle = (2 * Math.PI) - res_angle;
+        }
+
+        Vector sum = new Vector(res_magnitude, res_angle);
+        return sum;
+    }
+
+    public Vector subtract(Vector other){
+        Vector flip = new Vector(other.getValue(), (other.getAngle() + Math.PI));
+        return this.add(flip);
+    }
 }

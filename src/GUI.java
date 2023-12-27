017e88a9ec973f5da62ec3a7085a222762f30d6e
Xuan Pham <hpham024@uottawa.ca>
2023-12-26 23:53:58 -0500

    fixed alignment and width of menu panel

--- src/GUI.java
+++ src/GUI.java
@@ -3,24 +3,29 @@ import java.awt.*;
 
 public class GUI {
 
-    static JPanel createSidebar(){
+    static JPanel createSidebar(int width, int height){
         // sidebar
         JPanel sidebar = new JPanel();
         sidebar.setLayout(new BoxLayout(sidebar, BoxLayout.Y_AXIS));
+        sidebar.setSize(width, height);
 
-        JPanel sMenu = createMenu("Solution", false);
-        JPanel tMenu = createMenu("Titrant", true);
+        JPanel sMenu = createMenu("Solution", false, width, height/3);
+        JPanel tMenu = createMenu("Titrant", true, width, height/3);
+        JButton button = new JButton("Run");
 
         // add solution menu and titrant menu to sidebar
         sidebar.add(sMenu);
         sidebar.add(tMenu);
+        sidebar.add(button);
 
         return sidebar;
     }
 
-    static JPanel createMenu(String name, boolean naOption){
+    static JPanel createMenu(String name, boolean naOption, int width, int height){
         JPanel menu = new JPanel();
         menu.setLayout(new BoxLayout(menu, BoxLayout.Y_AXIS));
+        menu.setSize(width, height);
+
         JLabel mainLabel = new JLabel(name);
         menu.add(mainLabel);
 
@@ -43,6 +48,8 @@ public class GUI {
         // input field
         JLabel inputLabel = new JLabel("Relative Abundance:");
         JTextField tf = new JTextField(10);
+//        Dimension max = new Dimension(width, height/10);
+//        tf.setMaximumSize(max);
 
         menu.add(inputLabel);
         menu.add(tf);
@@ -53,12 +60,14 @@ public class GUI {
     public static void main(String[] args){
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize(screenSize.width / 2, screenSize.height / 2);
 
         // main sim window
 
-        JPanel sidebar = createSidebar();
+        JPanel sidebar = createSidebar(frame.getWidth()/10, frame.getHeight());
 
-        frame.getContentPane().add(sidebar);
+        frame.getContentPane().add(BorderLayout.EAST, sidebar);
         frame.setVisible(true);
 
     }
